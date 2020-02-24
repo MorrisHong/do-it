754fb0fa5b1b3fd6fd64711cd3a19f6cca1d3f34
gracelove <govlmo91@gmail.com>
2020-02-25 02:39:33 +0900

    / 과 /login 페이지로 오는 get요청 매핑

---
+++ src/main/java/kr/joyful/doit/web/MainController.java
@@ -0,0 +1,13 @@
+package kr.joyful.doit.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"/", "/login"})
+    public String entry() {
+        return "index";
+    }
+}
