f9be47f3240404a49cc075f06723c17272f39f3d
gracelove <govlmo91@gmail.com>
2020-02-24 22:08:07 +0900

    init

---
+++ src/main/java/kr/joyful/doit/DoItApplication.java
@@ -0,0 +1,13 @@
+package kr.joyful.doit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/joyful/doit/DoItApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.joyful.doit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
