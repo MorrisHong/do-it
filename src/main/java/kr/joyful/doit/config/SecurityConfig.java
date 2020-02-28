6ee4758fd4502f4866e59b1dd4b302c395af307f
gracelove <govlmo91@gmail.com>
2020-02-28 23:46:19 +0900

    /fonts/** ignoring

--- src/main/java/kr/joyful/doit/config/SecurityConfig.java
+++ src/main/java/kr/joyful/doit/config/SecurityConfig.java
@@ -28,6 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico");
+        web.ignoring().antMatchers("/fonts/**", "/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico");
     }
 }
