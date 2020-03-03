e498313ff402c43820f7e073b98a330752cc2fa0
sup2is <dev.sup2is@gmail.com>
2020-03-04 07:17:44 +0900

    Detail 페이지 수정 및 CardInputModal 완성

---
+++ frontend/src/views/CardInputModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">새로운 카드 추가</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Card Name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea
+                  name="Description"
+                  label="Description"
+                  outlined
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  import { EventBus } from '../main.js'
+
+  export default {
+    data: () => ({
+      dialog: false,
+    }),
+    created () {
+      var vm = this
+      EventBus.$on('cardInputModal', function (value) {
+        vm.dialog = value
+      })
+    }
+  }
+</script>
--- frontend/src/views/DetailPage.vue
+++ frontend/src/views/DetailPage.vue
@@ -7,64 +7,71 @@
         <v-btn color="primary" class="mt-1">초대하기</v-btn>
         <v-divider class="mx-4" vertical></v-divider>
         <v-btn color="primary" class="mt-1">설정</v-btn>
+        <v-divider class="mx-4" vertical></v-divider>
+        <v-btn color="primary" class="mt-1"><v-icon>mdi-plus</v-icon>새카드리스트 추가</v-btn>
       </v-flex>
       <v-divider class="ma-4"></v-divider>
-      <div class="row">
-        <div class="col-3">
-          <h3>Todo List</h3>
+
+      <draggable
+        v-model="cards"
+        tag="v-layout"
+        class="row wrap align-center sortable-list"
+      >
+
+      <v-flex
+        v-for="card in cards"
+        :key="card.index"
+        class="sortable row-v align-self-start"
+        xs4
+        pa-3
+      >
+        <v-card 
+          class="pa-3"
+        >
+          <h3>{{ card.title }}</h3>
           <draggable
             class="dragArea list-group"
-            :list="list1"
-            :group="{ name: 'people', pull: 'clone', put: false }"
+            :list="card.cardList"
             @change="log"
           >
             <div
               class="list-group-item"
-              v-for="element in list1"
+              v-for="element in card.cardList"
               :key="element.name"
             >
               {{ element.name }}
             </div>
           </draggable>
-        </div>
-
-        <div class="col-3">
-          <h3>In Progress</h3>
-          <draggable
-            class="dragArea list-group"
-            :list="list2"
-            group="people"
-            @change="log"
-          >
-            <div
-              class="list-group-item"
-              v-for="element in list2"
-              :key="element.name"
+          <v-card-actions>
+            <v-btn
+              text
+              color="deep-purple accent-4"
+              @click="cardInputModalOn()"
             >
-              {{ element.name }}
-            </div>
-          </draggable>
-        </div>
-
-        <rawDisplayer class="col-3" :value="list1" title="List 1" />
-
-        <rawDisplayer class="col-3" :value="list2" title="List 2" />
-      </div>
+              <v-icon>mdi-plus</v-icon>새 카드 추가하기
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      </draggable>
+      <card-input-modal/>
     </v-container>
   </v-content>
 </template>
 
 <script>
 
+import { EventBus } from '../main.js'
 import draggable from "vuedraggable";
-
+import CardInputModal from "@/views/CardInputModal.vue"
 
 export default {
   name: "clone",
   display: "Clone",
   order: 2,
   components: {
-    draggable
+    draggable,
+    'card-input-modal': CardInputModal
   },
   data() {
     return {
@@ -78,12 +85,96 @@ export default {
         { name: "Juan", id: 5 },
         { name: "Edgard", id: 6 },
         { name: "Johnson", id: 7 }
+      ],
+      list3: [
+        { name: "Juan", id: 5 },
+        { name: "Edgard", id: 6 },
+        { name: "Johnson", id: 7 }
+      ],
+      cards: [
+        {
+          title: "TodoList",
+          index: 1,
+          cardList: [
+            { name: "John234234", id: 1 },
+            { name: "Joao234234", id: 2 },
+            { name: "Jean22", id: 3 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+          ]
+        },
+        {
+          title: "InProgress2",
+          index: 2,
+          cardList: [
+            { name: "John11", id: 1 },
+            { name: "Joao##", id: 2 },
+            { name: "Jean#", id: 3 },
+            { name: "Gerard", id: 4 }
+          ]
+        },
+        {
+          title: "InProgress3",
+          index: 3,
+          cardList: [
+            { name: "John11", id: 1 },
+            { name: "Joao##", id: 2 },
+            { name: "Jean#", id: 3 },
+            { name: "Gerard", id: 4 }
+          ]
+        },
+        {
+          title: "InProgress4",
+          index: 4,
+          cardList: [
+            { name: "John11", id: 1 },
+            { name: "Joao##", id: 2 },
+            { name: "Jean#", id: 3 },
+            { name: "Gerard", id: 4 }
+          ]
+        },
+        {
+          title: "InProgress5",
+          index: 5,
+          cardList: [
+            { name: "John11", id: 1 },
+            { name: "Joao##", id: 2 },
+            { name: "Jean#", id: 3 },
+            { name: "Gerard", id: 4 }
+          ]
+        },
+        {
+          title: "TodoList",
+          index: 6,
+          cardList: [
+            { name: "John234234", id: 1 },
+            { name: "Joao234234", id: 2 },
+            { name: "Jean22", id: 3 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+            { name: "Gerard", id: 4 },
+          ]
+        },
       ]
     };
   },
   methods: {
     log: function(evt) {
       window.console.log(evt);
+    },
+    cardInputModalOn: function () {
+      EventBus.$emit('cardInputModal', true);
     }
   }
 };
