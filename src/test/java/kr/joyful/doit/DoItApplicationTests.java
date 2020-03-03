dfead05fd0981de76d51409ca3880c1447bbe21e
gracelove <govlmo91@gmail.com>
2020-03-04 01:06:51 +0900

    teamcontroller 테스트 추가, 깨진 테스트 보완

--- src/test/java/kr/joyful/doit/DoItApplicationTests.java
+++ src/test/java/kr/joyful/doit/DoItApplicationTests.java
@@ -2,8 +2,10 @@ package kr.joyful.doit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DoItApplicationTests {
 
     @Test
--- src/test/java/kr/joyful/doit/web/team/TeamControllerTest.java
+++ src/test/java/kr/joyful/doit/web/team/TeamControllerTest.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,7 +29,7 @@ class TeamControllerTest {
     MockMvc mockMvc;
 
     @Autowired
-    TeamRepository teamRepository;
+    UserDetailsService memberService;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -45,12 +48,15 @@ class TeamControllerTest {
 
     @Test
     @DisplayName("중복된 팀이름으로 만드려고할 때 배드리퀘스트 떠야함")
-    @WithUserDetails(userDetailsServiceBeanName = "memberService", value = "member1@example.com")
+//    @WithUserDetails(userDetailsServiceBeanName = "memberService", value = "member1@example.com")
     void failed_duplicate_team_name() throws Exception {
         //given
+        UserDetails userDetails = memberService.loadUserByUsername("member1@example.com");
+
         TeamCreateRequestDto dto = new TeamCreateRequestDto("teamA", "this is teamA");
 
         mockMvc.perform(post("/api/team")
+                .with(user(userDetails))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isCreated());
@@ -59,9 +65,36 @@ class TeamControllerTest {
         TeamCreateRequestDto dto2 = new TeamCreateRequestDto("teamA", "this is another teamA");
 
         mockMvc.perform(post("/api/team")
+                .with(user(userDetails))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(dto2)))
                 .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    @DisplayName("서로 다른 멤버가 같은 팀 이름으로 저장할 때 201떠야함")
+    void different_member_duplicate_team_name() throws Exception {
+        //given
+        UserDetails userDetails1 = memberService.loadUserByUsername("member1@example.com");
+        UserDetails userDetails2 = memberService.loadUserByUsername("member2@example.com");
+
+        TeamCreateRequestDto dto = new TeamCreateRequestDto("teamA", "this is teamA");
+
+        mockMvc.perform(post("/api/team")
+                .with(user(userDetails1))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isCreated());
+
+        //when & then
+        TeamCreateRequestDto dto2 = new TeamCreateRequestDto("teamA", "this is another teamA");
+
+        mockMvc.perform(post("/api/team")
+                .with(user(userDetails2))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto2)))
+                .andExpect(status().isCreated());
 
     }
 }
