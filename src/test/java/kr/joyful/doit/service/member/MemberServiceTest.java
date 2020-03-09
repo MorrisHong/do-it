f36541e7a943cd0fcb939efb997e29c7108796bc
gracelove <govlmo91@gmail.com>
2020-03-10 02:20:37 +0900

    memberService 테스트 보완

--- src/test/java/kr/joyful/doit/service/member/MemberServiceTest.java
+++ src/test/java/kr/joyful/doit/service/member/MemberServiceTest.java
@@ -1,37 +1,81 @@
 package kr.joyful.doit.service.member;
 
 import kr.joyful.doit.domain.member.Member;
+import kr.joyful.doit.domain.member.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
 
-    @Mock
     MemberService memberService;
+    MemberRepository memberRepository;
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository = mock(MemberRepository.class);
+        passwordEncoder = mock(PasswordEncoder.class);
+        memberService = new MemberService(memberRepository, passwordEncoder);
+    }
 
     @Test
-    void join() {
+    @DisplayName("패스워드 인코딩 잘됨")
+    void successFul_join() {
         Member member = Member.builder()
                 .email("mock@email.com")
                 .username("mockMember")
                 .password("1234")
                 .build();
 
-        given(memberService.join(member)).willReturn(1L);
+        when(memberRepository.save(member)).thenReturn(member);
+        when(passwordEncoder.encode(member.getPassword())).thenReturn("!@#");
 
-        Long memberId = memberService.join(member);
+        memberService.join(member);
+        assertEquals("!@#", member.getPassword());
+    }
 
-        given(memberService.findMemberById(memberId)).willReturn(member);
+    @Test
+    @DisplayName("이미 있는 username으로 회원가입시 MemberAlreadyExistsException 터짐")
+    void alreadyExistsUsername_memberJoin() {
+        String username = "mockMember";
+        Member member = Member.builder()
+                .email("mock@email.com")
+                .username(username)
+                .password("1234")
+                .build();
 
-        Member findMember = memberService.findMemberById(memberId);
+        given(memberRepository.findByUsername(username)).willReturn(Optional.of(member));
 
-        assertEquals(findMember.getEmail(), member.getEmail());
+        assertThrows(MemberAlreadyExistsException.class, () -> memberService.join(member));
+    }
+
+    @Test
+    @DisplayName("이미 있는 email로 회원가입시 MemberAlreadyExistsException 터짐")
+    void alreadyExistsEmail_memberJoin() {
+        String email = "mock@email.com";
+        Member member = Member.builder()
+                .email(email)
+                .username("mockMember")
+                .password("1234")
+                .build();
+
+        given(memberRepository.findByEmail(email)).willReturn(Optional.of(member));
+
+        assertThrows(MemberAlreadyExistsException.class, () -> memberService.join(member));
     }
 
 
