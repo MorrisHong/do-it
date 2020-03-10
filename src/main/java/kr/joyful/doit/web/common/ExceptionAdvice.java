e49d90438d7ba987adf746d80a8b0f5e75a2a0fa
sup2is <dev.sup2is@gmail.com>
2020-03-11 07:34:02 +0900

    userNotFoundException, teamAlreadyExistsException 핸들러 추가

--- src/main/java/kr/joyful/doit/web/common/ExceptionAdvice.java
+++ src/main/java/kr/joyful/doit/web/common/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package kr.joyful.doit.web.common;
 
+import kr.joyful.doit.service.member.MemberNotFoundException;
 import kr.joyful.doit.service.team.TeamAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler(TeamAlreadyExistsException.class)
+    @ExceptionHandler(MemberNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String userNotFoundException(TeamAlreadyExistsException e) {
+    public String userNotFoundException(MemberNotFoundException e) {
         return e.getMessage();
     }
+
+    @ExceptionHandler(TeamAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String teamAlreadyExistsException(TeamAlreadyExistsException e) {
+        return e.getMessage();
+    }
+
 
 }
