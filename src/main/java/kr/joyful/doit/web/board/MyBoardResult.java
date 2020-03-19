692b8a9c6997f66ae48ecd32dc87284ee6b76daa
gracelove <govlmo91@gmail.com>
2020-03-19 20:09:00 +0900

    MyBoardResult

--- src/main/java/kr/joyful/doit/web/board/MyBoardResult.java
+++ src/main/java/kr/joyful/doit/web/board/MyBoardResult.java
@@ -1,42 +1,58 @@
 package kr.joyful.doit.web.board;
 
 import kr.joyful.doit.domain.board.Board;
-import kr.joyful.doit.domain.team.Team;
 import kr.joyful.doit.web.result.ApiResult;
 import kr.joyful.doit.web.result.Result;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
 
 @Getter
 public class MyBoardResult {
 
     public static ResponseEntity<ApiResult> build(List<Board> boards) {
-        Map<String, List<BoardData>> map = new HashMap<>();
-        boards.forEach(board -> {
-            BoardData boardData = new BoardData(board);
-            map.computeIfAbsent(boardData.getTeamName(), aLong -> new ArrayList<>()).add(boardData);
-        });
-        return Result.ok(ApiResult.blank().add("myBoard", map));
+        Map<String, List<Board>> collect = boards.stream().collect(groupingBy(board -> board.getTeam().getName()));
+        Set<String> strings = collect.keySet();
+        List<BoardData> result = new ArrayList<>();
+        for (String string : strings) {
+            List<Board> boards1 = collect.get(string);
+            result.add(new BoardData(boards1));
+        }
+        return Result.ok(ApiResult.blank().add("myBoard", result));
     }
 
     @Getter
     private static class BoardData {
-        private Long boardId;
-        private String title;
-        private String description;
         private String teamName;
+        private List<BoardDataDetail> details = new ArrayList<>();
 
-        BoardData(Board board) {
-            this.boardId = board.getId();
-            this.title = board.getTitle();
-            this.description = board.getDescription();
-            this.teamName = board.getTeam().getName();
+        public BoardData(List<Board> boards) {
+            boards.forEach(board -> {
+                this.teamName = board.getTeam().getName();
+                this.details.add(new BoardDataDetail(board));
+            });
         }
+
+        @Getter
+        private static class BoardDataDetail {
+            private Long boardId;
+            private String title;
+            private String description;
+            private String teamName;
+
+            public BoardDataDetail(Board board) {
+                this.boardId = board.getId();
+                this.title = board.getTitle();
+                this.description = board.getDescription();
+                this.teamName = board.getTeam().getName();
+            }
+        }
+
     }
 }
