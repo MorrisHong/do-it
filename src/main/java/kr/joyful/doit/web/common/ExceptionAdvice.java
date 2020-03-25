80ea711bd80e520e48bfb2a944810f1d3143fa1f
gracelove <govlmo91@gmail.com>
2020-03-25 14:02:45 +0900

    ExceptionAdvice.class - 익셉션 공통화 처리

--- src/main/java/kr/joyful/doit/web/common/ExceptionAdvice.java
+++ src/main/java/kr/joyful/doit/web/common/ExceptionAdvice.java
@@ -1,7 +1,14 @@
 package kr.joyful.doit.web.common;
 
+import kr.joyful.doit.service.board.BoardAlreadyExistsException;
+import kr.joyful.doit.service.board.BoardNotFoundException;
+import kr.joyful.doit.service.card.CardNotFoundException;
+import kr.joyful.doit.service.cardList.CardListNotFoundException;
+import kr.joyful.doit.service.member.MemberAlreadyExistsException;
 import kr.joyful.doit.service.member.MemberNotFoundException;
 import kr.joyful.doit.service.team.TeamAlreadyExistsException;
+import kr.joyful.doit.service.team.TeamNotFoundException;
+import kr.joyful.doit.web.member.NotOwnerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,9 +17,47 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
+    @ExceptionHandler(BoardNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String boardNotFoundException(BoardNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(CardNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String cardNotFoundException(CardNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(CardListNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String cardListNotFoundException(CardListNotFoundException e) {
+        return e.getMessage();
+    }
+
+
+
+    @ExceptionHandler(BoardAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String boardAlreadyExistsException(BoardAlreadyExistsException e) {
+        return e.getMessage();
+    }
+
     @ExceptionHandler(MemberNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String userNotFoundException(MemberNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(MemberAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String memberAlreadyExistsException(MemberAlreadyExistsException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String teamNotFoundException(TeamNotFoundException e) {
         return e.getMessage();
     }
 
@@ -22,5 +67,9 @@ public class ExceptionAdvice {
         return e.getMessage();
     }
 
-
+    @ExceptionHandler(NotOwnerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String notOwnerException(NotOwnerException e) {
+        return e.getMessage();
+    }
 }
