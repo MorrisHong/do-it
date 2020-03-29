c49091a43d17ab6e02dd550a2c66300b79afa527
gracelove <govlmo91@gmail.com>
2020-03-29 12:43:22 +0900

    CardListController - 리턴타입변경

--- src/main/java/kr/joyful/doit/web/cardList/CardListController.java
+++ src/main/java/kr/joyful/doit/web/cardList/CardListController.java
@@ -1,8 +1,11 @@
 package kr.joyful.doit.web.cardList;
 
 import kr.joyful.doit.domain.board.Board;
+import kr.joyful.doit.domain.cardList.CardList;
 import kr.joyful.doit.service.board.BoardService;
 import kr.joyful.doit.service.cardList.CardListService;
+import kr.joyful.doit.web.result.ApiResult;
+import kr.joyful.doit.web.result.Result;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,17 +20,19 @@ public class CardListController {
     private final CardListService cardListService;
     private final BoardService boardService;
 
-    @PostMapping("/api/card-list")
-    public ResponseEntity<?> addCardList(@RequestBody CardListAddRequestDto dto) throws URISyntaxException {
+    @PostMapping("/api/card-lists")
+    public ResponseEntity<ApiResult> addCardList(@RequestBody CardListAddRequestDto dto) throws URISyntaxException {
         Long boardId = dto.getBoardId();
         Board findBoard = boardService.findById(boardId);
-        Long cardListId = cardListService.addCardList(dto.toEntity(findBoard));
-        return ResponseEntity.created(new URI("/api/card-list/"+cardListId)).build();
+        cardListService.addCardList(dto.toEntity(findBoard));
+        return Result.created();
     }
 
     //todo : ENTITY 직접반환하지 말고 dto로 바꿔야함. (CardListResponseDto.class)
     @GetMapping("/api/card-list/{cardListId}")
-    public ResponseEntity<?> getCardListById(@PathVariable Long cardListId) {
-        return ResponseEntity.ok(cardListService.findById(cardListId));
+    public ResponseEntity<ApiResult> getCardListById(@PathVariable Long cardListId) {
+        CardList cardList = cardListService.findById(cardListId);
+
+        return null;
     }
 }
